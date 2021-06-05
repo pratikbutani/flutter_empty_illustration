9042028f72fb28c32b2ea320759dfe63bbaf2269
Pratik Butani <pratik13butani@gmail.com>
2021-06-05 18:34:25 +0530

    Done for No Internet

--- lib/flutter_empty_illustration.dart
+++ lib/flutter_empty_illustration.dart
@@ -1,6 +1,7 @@
 library flutter_empty_illustration;
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class NoInternetWidget extends StatelessWidget {
   const NoInternetWidget({Key key}) : super(key: key);
@@ -8,31 +9,23 @@ class NoInternetWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
+      color: Colors.white,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            // mainAxisSize: MainAxisSize.max,
-            children: [
-              Icon(
-                Icons.wifi_off_rounded,
-                size: 150,
-                color: Colors.black54,
-              ),
-              SizedBox(height: 20, width: 20),
-              Text(
-                "Internet Connection Not Available, Try Again!",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18.0, color: Colors.black54, fontWeight: FontWeight.bold),
-              ),
-            ],
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Lottie.asset('assets/no_internet_connection.json',package: 'flutter_empty_illustration', width: 400, height: 300),
+          SizedBox(height: 20, width: 20),
+          Text(
+            "Internet Connection Not Available,\nTry Again!",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0, color: Colors.black54, fontWeight: FontWeight.bold),
           ),
-        ));
+        ],
+      ),
+    ));
   }
 }
-
-
-
