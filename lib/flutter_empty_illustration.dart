c0482300c8bacfac3a2e626bfd533d654fd42275
Pratik Butani <pratik13butani@gmail.com>
2021-06-07 14:28:44 +0530

    width changes

--- lib/flutter_empty_illustration.dart
+++ lib/flutter_empty_illustration.dart
@@ -9,23 +9,25 @@ class NoInternetWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
         child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Lottie.asset('assets/no_internet_connection.json',package: 'flutter_empty_illustration', width: 400, height: 300),
-          SizedBox(height: 20, width: 20),
-          Text(
-            "Internet Connection Not Available,\nTry Again!",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18.0, color: Colors.black54, fontWeight: FontWeight.bold),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Lottie.asset('assets/no_internet_connection.json', package: 'flutter_empty_illustration', width: 450, fit: BoxFit.cover),
+              SizedBox(height: 20, width: 20),
+              Text(
+                "Internet Connection Not Available,\nTry Again!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18.0, color: Colors.black54, fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
